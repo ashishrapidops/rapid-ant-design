527168fba4520784e6f188b44bf09f932fdf06da
Ashish Malvaniya <ashish.malvania@rapidops.com>
2023-02-08 13:38:02 +0530

    Published by Ant Design 7fcb262b7e620ad253719d05ccffc91dcbf975b7 (HEAD -> refs/heads/main, refs/remotes/origin/main, refs/remotes/origin/HEAD) Merge pull request #1 from ashishrapidops/create-version-release

---
+++ es/select/style/index.js
@@ -0,0 +1,5 @@
+import '../../style/index.less';
+import './index.less';
+// style dependencies
+import '../../empty/style';
+// deps-lint-skip: form
---
+++ es/upload/style/css.js
@@ -0,0 +1,7 @@
+import '../../style/index.css';
+import './index.css';
+// style dependencies
+// deps-lint-skip: form
+import '../../button/style/css';
+import '../../progress/style/css';
+import '../../tooltip/style/css';
